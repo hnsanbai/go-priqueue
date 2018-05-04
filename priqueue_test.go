08073ae5090edeee1d97f9761ba66ab596bc214f
haibin peng <exiaojiu@163.com>
2018-05-04 18:27:15 +0800

    测试代码

--- priqueue_test.go
+++ priqueue_test.go
@@ -3,30 +3,72 @@ package priqueue
 import (
 	"testing"
 	"math/rand"
+	"runtime"
+	"sync"
+	"time"
+	"sort"
 )
-var lastw int
-
 func TestPush(t *testing.T) {
-	notices := make(Notices, 0,10)
-	t.Log(notices)
-	for len(notices) < 60 {
-		d := rand.Intn(1000-100)+100
-		notice := &Notice{
-			Weight:			d,
-			PhoneNumber:	13000000000,
-			Message:		"我是通知消息",
-		}
-		t.Logf("add %d\n", d)
-		notices.Push(notice)
+	runtime.GOMAXPROCS(4)
+	
+	notices	:= make(Notices, 0, 180)
+	var wg sync.WaitGroup
+	var lock sync.Mutex
+	
+	wg.Add(4)
+	
+	for i := 0; i < 3; i++ {
+		go func() {
+			for j := 0; j < 60; j++ {
+				d := rand.Intn(10000-100)+100
+				notice := &Notice{
+					Weight:			d,
+					PhoneNumber:	13000000000,
+					Message:		"我是通知消息",
+				}
+				t.Logf("add %d\n", d)
+				
+				lock.Lock()
+				notices.Push(notice)
+				lock.Unlock()
+			}
+			wg.Done()
+		}()
 	}
 	
-	for len(notices) > 0 {
-		n := notices.Pop()
-		ne := n.(*Notice)
-		t.Logf("Pop %d\n", ne.Weight)
-		
-		if lastw > ne.Weight {
-			t.Fatalf("expected weight: %d and %d", lastw, ne.Weight)
+	go func() {
+		counter := 0
+		for {
+			if counter > 10 {
+				break
+			}
+			
+			lock.Lock()
+			var ne *Notice
+			index := 0
+			length := len(notices)
+			if length > 0 {
+				n := notices.Pop()
+				ne = n.(*Notice)
+				length = len(notices);
+				index = sort.Search(len(notices), func(i int) bool { return notices[i].Weight > ne.Weight })
+			}
+			lock.Unlock()
+			
+			if length > 0 {
+				if index < length {
+					t.Fatalf("expected weight: %d, index: %d, length: %d", ne.Weight, index, length )
+				}
+				t.Logf("Pop %d\n", ne.Weight)
+			} else {
+				time.Sleep(100 * time.Millisecond)
+				counter++
+			}
+			
 		}
-	}
+		wg.Done()
+		t.Log("end-----------------------")
+	}()
+	
+	wg.Wait()
 }
